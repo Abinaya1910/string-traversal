b8806cbe0597c7b9ee17f9e4ad60c34f1ef8d1b6
Abinaya1910 <124350817+Abinaya1910@users.noreply.github.com>
2023-08-07 15:46:05 +0530

    Add files via upload

---
+++ vowel count.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>  
+void main ()  
+{  
+    char s[11] = "javatpoint";  
+    int i = 0;   
+    int count = 0;  
+    while(i<11)  
+    {  
+        if(s[i]=='a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'u' || s[i] == 'o')  
+        {  
+            count ++;  
+        }  
+        i++;  
+    }  
+    printf("The number of vowels %d",count);  
+}  
